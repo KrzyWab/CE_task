3928f6d1e32f68bf1fc1d18e510d7063cae51743
KW <krzysiekwab@gmail.com>
2023-05-27 00:22:28 +0200

    Small refactoring for blinkThread
    For final device this thread will be not used, so will keep it as
    it is in current stateto not make new files

--- src/blinkThread.c
+++ src/blinkThread.c
@@ -1,37 +1,52 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
 
 #include "blinkThread.h"
 
-#define SLEEP_MSEC	3000
+#define TEST_SLEEP_MSEC		3000
 
-/* build error on this step means there is no driver for PWM on LED0 for this board */
+LOG_MODULE_REGISTER(blinkngLED);
+
+/* build error on this step means there is no driver for PWM on LED1 for this board */
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+
+static int blinkLed_init(void);
+static void blinkLed_test(void);
 
 void blinkThread_entry(void)
 {
-	int ret;
-
-	if (!gpio_is_ready_dt(&led1)) {
-		return;
+	if(blinkLed_init())
+	{
+		k_sleep(K_FOREVER);
 	}
 
-	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return;
-	}
-
-	printk("Blinking LED thread ready\n");
-
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led1);
-		if (ret < 0) {
-			return;
-		}
-		k_msleep(SLEEP_MSEC);
-	}
-
-	return;
+	blinkLed_test();
 }
 
+static int blinkLed_init(void)
+{
+	if (!gpio_is_ready_dt(&led1)) {
+		LOG_ERR("GPIO for LED1 is not ready");
+		return -1;
+	}
+	if (gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE) < 0) {
+		LOG_ERR("Cannot active GPIO for LED1");
+		return -1;
+	}
+	LOG_INF("LED1 initialized successfully");
+
+	return 0;
+}
+
+static void blinkLed_test(void)
+{
+	while (1) {
+		if (gpio_pin_toggle_dt(&led1))
+		{
+			LOG_ERR("Cannot toggle GPIO state for LED1");
+			k_sleep(K_FOREVER);
+		}
+		k_msleep(TEST_SLEEP_MSEC);
+	}
+}
