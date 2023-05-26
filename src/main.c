5c269f59fe536c6198c6e4c2213f3bfe7ca7124c
KW <krzysiekwab@gmail.com>
2023-05-27 00:26:23 +0200

    Removed blinkThread init and from build process

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
 #include <zephyr/kernel.h>
 
-#include "blinkThread.h"
 #include "displayThread.h"
-
-K_THREAD_DEFINE(blinkThread_id, BLINK_THREAD_STACKSIZE,
-				blinkThread_entry, NULL, NULL, NULL,
-				BLINK_THREAD_PIORITY, 0, 0);
 
 K_THREAD_DEFINE(displayThread_id, DISPLAY_THREAD_STACKSIZE,
 				displayThread_entry, NULL, NULL, NULL,
